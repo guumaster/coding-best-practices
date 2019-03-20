1c34bfc53e4fdeebda3e6209a99569e50a6ecf63
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 21:36:10 +0100

    Update side_effects.js

--- code/side_effects.js
+++ code/side_effects.js
@@ -3,12 +3,12 @@ let name = 'Rick Sanchez'
 const splitName = () => name = name.split(" ")
 
 splitName()
-console.log(name); // ['Rick', 'Sanchez']
+console.log(name) // ['Rick', 'Sanchez']
 
 // Good:
 const splitName = (name) => name = name.split(" ")
 
 let name = 'Rick Sanchez'
 let newName = splitName(name)
-console.log(name); // ['Rick Sanchez']
-console.log(newName); // ['Rick', 'Sanchez']
+console.log(name) // ['Rick Sanchez']
+console.log(newName) // ['Rick', 'Sanchez']
