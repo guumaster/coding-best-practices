a0623a7047ab7dcc628cc21661c624260cfd86d7
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 11:00:18 +0100

    Update comments_bad1.js

--- code/comments_bad1.js
+++ code/comments_bad1.js
@@ -1,9 +1,13 @@
-// Magic. Do not touch.
-
-// drunk, fix later
+// Get the count
+function getCount() {
+// ... long function body
+} // end getCount
 
 // somedev1 -  6/7/02 Adding temporary tracking of Login screen
 // somedev2 -  5/22/07 Temporary my ass
+
+// Magic. Do not touch.
+// drunk, fix later
 
 // When I wrote this, only God and I understood what I was doing
 // Now, God only knows
