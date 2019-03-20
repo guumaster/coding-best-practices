10696fde6f1e6e093903de6db07f2be178aa27da
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 10:47:10 +0100

    Update readability_good.js

--- code/readability_good.js
+++ code/readability_good.js
@@ -1,11 +1,10 @@
-function countOccurence(stack, hey) {
-  const total = stack.split("")
-    .reduce((total, actual) => {
-      if (stack[actual] === hey) {
+function countLetterOccurence(word, letter) {
+  const letters = word.split("")
+    
+  return letters.reduce((total, actual) => {
+      if (stack[actual] === letter) {
         total += 1
       }
       return total
     }, 0)
-  
-  return total
 }
