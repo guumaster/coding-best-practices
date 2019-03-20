99414b301ea91bb7808ca6f03b040532e40bfacf
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 21:25:22 +0100

    Create consistency.js

---
+++ code/consistency.js
@@ -0,0 +1,7 @@
+// Bad:
+const someVar = "ThisIsCamelCase"
+const some_var = "ThisIsSnakeCase"
+const SOME_VAR = "ThisIsUpperCase"
+
+// Same rules for function names
+// and file names: snake_case.js camelCase.js kebab-case.js
