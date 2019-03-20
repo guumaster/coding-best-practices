fdd7c905b8259b7c2ac1fd2e024690e4f295ed42
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 14:00:52 +0100

    Create side_effects.js

---
+++ code/side_effects.js
@@ -0,0 +1,14 @@
+// Bad:
+let name = 'Rick Sanchez'
+const splitName = () => name = name.split(" ")
+
+splitName()
+console.log(name); // ['Rick', 'Sanchez']
+
+// Good:
+const splitName = (name) => name = name.split(" ")
+
+let name = 'Rick Sanchez'
+let newName = splitName(name)
+console.log(name); // ['Rick Sanchez']
+console.log(newName); // ['Rick', 'Sanchez']
