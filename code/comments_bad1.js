c69389efbe9505b0ecf34f9238dafea73a5f7f4d
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 10:57:14 +0100

    Create comments_bad1.js

---
+++ code/comments_bad1.js
@@ -0,0 +1,10 @@
+// Magic. Do not touch.
+
+// drunk, fix later
+
+// somedev1 -  6/7/02 Adding temporary tracking of Login screen
+// somedev2 -  5/22/07 Temporary my ass
+
+// When I wrote this, only God and I understood what I was doing
+// Now, God only knows
+
