8d291c857a3680ea92eeb6f1709d38c096ac2f13
Gustavo <guumaster@users.noreply.github.com>
2019-03-20 13:56:42 +0100

    Create one_thing.js

---
+++ code/one_thing.js
@@ -0,0 +1,16 @@
+// Bad:
+function createFile(name, temp) {
+  if (temp) {
+    fs.create(`./temp/${name}`)
+  } else {
+    fs.create(name)
+  }
+}
+
+// Good: 
+function createFile(name) {
+  fs.create(name)
+}
+function createTempFile(name) {
+  createFile(`./temp/${name}`)
+}
