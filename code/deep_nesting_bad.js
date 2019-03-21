72c6035ce941c10f6f9ba71bb973eb2c9f499558
Gustavo <guumaster@users.noreply.github.com>
2019-03-21 11:24:47 +0100

    Update deep_nesting_bad.js

--- code/deep_nesting_bad.js
+++ code/deep_nesting_bad.js
@@ -3,7 +3,7 @@ function do_stuff() {
   if (is_writable(folder)) {
     if (fp = fopen(file_path,'w')) {
       if (stuff = get_some_stuff()) {
-        // ... 
+        // FUNCTION BODY HERE 
       } else {
         return false
       }
