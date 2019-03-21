508dab4f6569459c2b7454549320cc267b8617ba
Gustavo <guumaster@users.noreply.github.com>
2019-03-21 11:03:53 +0100

    Create deep_nesting_refactor.js

---
+++ code/deep_nesting_refactor.js
@@ -0,0 +1,15 @@
+function do_stuff() {
+  // ...
+  if (!is_writable(folder)) {
+    return false
+  }
+  fp = fopen(file_path,'w')
+  if (!fp) {
+    return false
+  }
+  if (stuff != get_some_stuff()) {
+    return false
+  }
+  
+  // ... 
+}
