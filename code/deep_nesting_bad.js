23001ece8516825f663f58686cda10f23169f474
Gustavo <guumaster@users.noreply.github.com>
2019-03-21 11:01:48 +0100

    Create deep_nesting_bad.js

---
+++ code/deep_nesting_bad.js
@@ -0,0 +1,16 @@
+function do_stuff() {
+// ...
+    if (is_writable(folder)) {
+        if (fp = fopen(file_path,'w')) {
+            if (stuff = get_some_stuff()) {
+               // ... 
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
